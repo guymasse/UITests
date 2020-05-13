1e5da4d45202fdee192de3e7255396b144f6662d
Guy Masse <guy.masse@tylertech.com>
2020-05-13 16:56:48 -0600

    Added IDisposable interface

--- UITests/TestSetup/SelectDriver.cs
+++ UITests/TestSetup/SelectDriver.cs
@@ -1,10 +1,10 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using System;
 
 namespace UITests.TestSetup
 {
-    class SelectDriver
+    class SelectDriver :IDisposable
     {
         public IWebDriver Driver { get; private set; }
 
