4ca4122653b0af3aec36ca8e61085d0722fe9593
Guy Masse <guy.masse@tylertech.com>
2020-05-22 09:13:32 -0600

    Completed the Birth certiificate wizard test

--- UITests/BirthCertificateWizard.cs
+++ UITests/BirthCertificateWizard.cs
@@ -31,13 +31,45 @@ namespace UITests
                 // Step 2 of wizard
                 birthCertificatePage.Step2FirstName.SendKeys("MATEO");
                 birthCertificatePage.Step2LastName.SendKeys("VIQUEZ");
-                birthCertificatePage.Step2DateOfBirthe.SendKeys("10/16/2014");
-                birthCertificatePage.Step2CityOfBirthe.SendKeys("TEMECULA");
+                birthCertificatePage.Step2DateOfBirth.SendKeys("10/16/2014");
+                birthCertificatePage.Step2CityOfBirth.SendKeys("TEMECULA");
                 birthCertificatePage.NextButton.Click();
-                WebPageDelay.Pause();
+
+                // Assert
+                Assert.AreEqual("Requestor Information", birthCertificatePage.Step3PageText.Text);
+
+                // Step 3 of wizard
+                birthCertificatePage.Step3FullName.SendKeys("ALVARO JOSH VIQUEZ");
+                birthCertificatePage.Step3StreetAddress.SendKeys("41402 WILLOW RUN ROAD");
+                birthCertificatePage.Step3City.SendKeys("TEMECULA");
+                birthCertificatePage.Step3State.SendKeys("CA");
+                birthCertificatePage.Step3Zip.SendKeys("90245");
+                birthCertificatePage.SetRelationship("PARENT");
+                birthCertificatePage.Step3IdNumber.SendKeys("ID-12345");
+                birthCertificatePage.Step3PhoneNumber.SendKeys("303-555-8888");
+                birthCertificatePage.Step3NumberOfCopies.SendKeys("1");
+                birthCertificatePage.NextButton.Click();
+
+                // Assert
+                Assert.AreEqual("Review Request", birthCertificatePage.Step4PageText.Text);
+
+                WebPageDelay.Pause(5000);
+
+                // Step 4 Complete
+                birthCertificatePage.CompleteButton.Click();
+
+                // Assert
+                Assert.AreEqual("Checkout", birthCertificatePage.CheckoutPageText.Text);
+
+                // Checkout
+                birthCertificatePage.SetPaymentMethod("In Office");
+                birthCertificatePage.CustomerName.SendKeys("ALVARO JOSH VIQUEZ");
+                birthCertificatePage.PlaceOrderButton.Click();
+                WebPageDelay.Pause(5000);
 
                 // assert
 
+                Assert.AreEqual("Thank you for your order.", birthCertificatePage.ConfirmationText.Text);
 
             }
         }
--- UITests/PageObjectModels/BirthCertificatePage.cs
+++ UITests/PageObjectModels/BirthCertificatePage.cs
@@ -29,9 +29,17 @@ namespace UITests.PageObjectModels
         // Next Button
         public IWebElement NextButton =>
             Wait.Until(e => e.FindElement(By.XPath("//a[@class='ss-right ui-link ui-btn ui-btn-a ui-icon-arrow-r ui-btn-icon-right ui-btn-inline ui-shadow ui-corner-all'][contains(.,'Next')]")));
-        // Next Button
+        // Previous Button
         public IWebElement PreviousButton =>
             Wait.Until(e => e.FindElement(By.XPath("//a[@class='ss-right ui-link ui-btn ui-btn-a ui-icon-arrow-l ui-btn-icon-left ui-btn-inline ui-shadow ui-corner-all'][contains(.,'Previous')]")));
+
+        // Complete Button
+        public IWebElement CompleteButton =>
+            Wait.Until(e => e.FindElement(By.XPath("//a[@class='ss-right ui-link ui-btn ui-btn-b ui-icon-check ui-btn-icon-right ui-btn-inline ui-shadow ui-corner-all'][contains(.,'Complete')]")));
+
+        // Place Order Button
+        public IWebElement PlaceOrderButton =>
+            Wait.Until(e => e.FindElement(By.XPath("//a[@class='ss-right ui-link ui-btn ui-btn-b ui-icon-check ui-btn-icon-right ui-btn-inline ui-shadow ui-corner-all'][contains(.,'Place your Order')]")));
 
         // STEP 1
         // I am radio button (The registrant)
@@ -50,11 +58,63 @@ namespace UITests.PageObjectModels
         public IWebElement Step2LastName =>
             Wait.Until(e => e.FindElement(By.Id("field_BirthLastName")));
 
-        public IWebElement Step2DateOfBirthe =>
+        public IWebElement Step2DateOfBirth =>
             Wait.Until(e => e.FindElement(By.Id("field_DateofBirth")));
 
-        public IWebElement Step2CityOfBirthe =>
+        public IWebElement Step2CityOfBirth =>
             Wait.Until(e => e.FindElement(By.Id("field_CityofBirth")));
+
+        // STEP 3
+        // Text on page
+        public IWebElement Step3PageText =>
+            Wait.Until(e => e.FindElement(By.XPath("//h1[contains(.,'Requestor Information')]")));
+
+        // Step 3 Required fields
+        public IWebElement Step3FullName =>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorName")));
+
+        public IWebElement Step3StreetAddress =>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorReturn2_DOT_Address1")));
+
+        public IWebElement Step3City=>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorReturn2_DOT_City")));
+
+        public IWebElement Step3State =>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorReturn2_DOT_State")));
+
+        public IWebElement Step3Zip =>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorReturn2_DOT_Zip")));
+
+        public IWebElement Step3Relationship =>
+            Wait.Until(e => e.FindElement(By.Id("field_RelationshipToPerson")));
+
+        public IWebElement Step3IdNumber =>
+            Wait.Until(e => e.FindElement(By.Id("field_IDNumber")));
+
+        public IWebElement Step3PhoneNumber =>
+            Wait.Until(e => e.FindElement(By.Id("field_RequestorPhone")));
+
+        public IWebElement Step3NumberOfCopies =>
+            Wait.Until(e => e.FindElement(By.Id("field_NumberOfCopies")));
+
+        // STEP 4
+        // Text on page
+        public IWebElement Step4PageText =>
+            Wait.Until(e => e.FindElement(By.XPath("//h1[contains(.,'Review Request')]")));
+
+        // Ceckout
+        // Text on page
+        public IWebElement CheckoutPageText =>
+            Wait.Until(e => e.FindElement(By.XPath("//h1[contains(.,'Checkout')]")));
+
+        public IWebElement PaymentMethod =>
+            Wait.Until(e => e.FindElement(By.Id("field_PayMethod")));
+
+        public IWebElement CustomerName =>
+            Wait.Until(e => e.FindElement(By.Id("field_Name")));
+
+        public IWebElement ConfirmationText =>
+            Wait.Until(e => e.FindElement(By.XPath("//b[contains(.,'Thank you for your order.')]")));
 
 
         public void NavigateTo()
@@ -70,6 +130,20 @@ namespace UITests.PageObjectModels
             {
                 throw new Exception($"Failed to load page. Page Url = {Driver.Url.Substring(0, 66)} Page Source: \r\n  {Driver.PageSource}");
             }
+        }
+
+        public void SetRelationship(string type)
+        {
+            SelectElement idRelationType = new SelectElement(Step3Relationship);
+            idRelationType.SelectByText(type);
+            WebPageDelay.Pause();
+        }
+
+        public void SetPaymentMethod(string method)
+        {
+            SelectElement paymentMethodType = new SelectElement(PaymentMethod);
+            paymentMethodType.SelectByText(method);
+            WebPageDelay.Pause();
         }
 
 
